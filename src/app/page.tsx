972f26190737a0fd9f71c3cc4f67908eea598ed6
ManahilNadeem <163245288+nadeemtahir@users.noreply.github.com>
2025-02-16 03:04:56 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,16 @@
-"use client";
-
-import { motion } from "framer-motion";
+import { FAQ } from "./component/faq";
 import { NeoAIShowcase } from "./component/features";
 import { Hero } from "./component/hero";
-import Price from "./component/price";
-import { FAQ } from "./component/faq";
+import PricingSection from "./component/price";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-950 text-white">
-      {/* Hero Section with Animation */}
-      <motion.section
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6 }}
-      >
-        <Hero />
-      </motion.section>
-
-      {/* Features Section */}
-      <motion.section
-        className="mt-16"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6, delay: 0.2 }}
-      >
-        <NeoAIShowcase />
-      </motion.section>
-
-      {/* Pricing Section */}
-      <motion.section
-        className="mt-16"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6, delay: 0.4 }}
-      >
-        <Price />
-      </motion.section>
-      <motion.section
-        className="mt-16"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6, delay: 0.4 }}
-      >
-        <FAQ />
-      </motion.section>
-    </main>
+    <>
+      <Hero />
+      <NeoAIShowcase />
+      <PricingSection/>
+      <FAQ/>
+    </>
   );
 }
+
